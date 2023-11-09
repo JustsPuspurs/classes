04677fe8bffe31ddbc1c7f668ab418ed2e56c284
ipb21.j.puspurs <ipb21.j.puspurs@pjvt.local>
2023-11-09 15:23:33 +0200

    gejs

---
+++ Car.php
@@ -0,0 +1,21 @@
+<?php
+
+class Car {
+    public $brand;
+
+    public function __construct($brand){
+        $this->brand = $brand;
+        echo $this->brand . " is alive <br>";
+    }
+    public function __destruct(){
+        echo $this->brand . " is dead <br>";
+    }
+    
+    static function makeNoise(){
+        echo "Beep BEEEEEEP!!! <br>";
+    }
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include("Car.php");
+
+echo"HI ";
+
+$myCar = new Car("Dacia");
+// $myCar2 = new Car("Apple Car");
+Car::makeNoise();
+
+$myCar->makeNoise();
+
+
+?>
